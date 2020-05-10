c46e7ecfa170a71c63ce8a133fc03cd9e395c416
Taeyoon Kim <tykimseoul@gmail.com>
2020-05-10 17:36:39 +0900

    question 3-1 passed

--- level3/q1/solution.py
+++ level3/q1/solution.py
@@ -7,9 +7,13 @@ def solution(x, y):
     gen = 0
     while min(x, y) != 1:
         if x > y:
+            if x % y == 0:
+                return 'impossible'
             gen += x // y
             x %= y
         elif x < y:
+            if y % x == 0:
+                return 'impossible'
             gen += y // x
             y %= x
         else:
